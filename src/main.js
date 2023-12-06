0c730cc21606d30b120a04174d76163b55d7b24b
sas-miles <132398659+sas-miles@users.noreply.github.com>
2023-12-05 21:42:00 -0500

    spline barba toggle

--- src/main.js
+++ src/main.js
@@ -2,48 +2,48 @@ import { Application } from '@splinetool/runtime';
 import barba from '@barba/core';
 import { gsap } from "gsap";
 
+let spline, obj;
+let isToggled = false;
 
-const canvas = document.getElementById('canvas3d');
-const spline = new Application(canvas);
+// Initialize Spline only once
+function initSpline() {
+    if (spline) return; // Check if Spline is already initialized
 
-spline.load('https://prod.spline.design/zBBaOmVltDuUag17/scene.splinecode')
-.then(() => {
-    const obj = spline.findObjectById('1abf83a1-5d8c-4819-a952-7bcd8afbcb11');
-    const textLink = document.querySelector('.click-text');
+    const canvas = document.getElementById('canvas3d');
+    spline = new Application(canvas);
 
-    let isToggled = false; // State to track the toggle
+    spline.load('https://prod.spline.design/zBBaOmVltDuUag17/scene.splinecode')
+    .then(() => {
+        obj = spline.findObjectById('1abf83a1-5d8c-4819-a952-7bcd8afbcb11');
+    });
+}
 
-    if (textLink) {
-        textLink.addEventListener('click', () => {
-            // Toggle the state
-            isToggled = !isToggled;
-
-            // Perform action based on the toggle state
-            if (isToggled) {
-                // Trigger the event
-                spline.emitEvent('mouseDown', obj.name);
-            } else {
-                // Reverse the event
-                spline.emitEventReverse('mouseDown', obj.name);
-            }
-        });
-    } else {
-        console.error('Text link element not found');
+// Handle click events using event delegation
+function handleClick(event) {
+    if (event.target.classList.contains('click-text')) {
+        isToggled = !isToggled;
+        if (isToggled) {
+            spline.emitEvent('mouseDown', obj.name);
+        } else {
+            spline.emitEventReverse('mouseDown', obj.name);
+        }
     }
-});
+}
 
+// Attach event listener to a static parent element for delegation
+const staticParent = document.body; // You can choose any static parent element
+staticParent.addEventListener('click', handleClick);
+
+// Initialize Barba.js with transitions
 barba.init({
     transitions: [{
-      name: 'opacity-transition',
-      leave(data) {
-        return gsap.to(data.current.container, {
-          opacity: 0
-        });
-      },
-      enter(data) {
-        return gsap.from(data.next.container, {
-          opacity: 0
-        });
-      }
+        leave(data) {
+            return gsap.to(data.current.container, { opacity: 0 });
+        },
+        enter(data) {
+            return gsap.from(data.next.container, { opacity: 0 });
+        }
     }]
-  });
+});
+
+initSpline(); // Initial call to setup Spline
