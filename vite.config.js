476f9b089736b57a330be1b37ce0823adaedca1b
sas-miles <132398659+sas-miles@users.noreply.github.com>
2023-12-06 14:09:07 -0500

    removed chunking to fix cors issue

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
 
-// vite.config.js
 export default defineConfig({
-  plugins: [glsl()],
-  server: {
-    host: 'localhost',
-    cors: '*',
-    hmr: {
-      host: 'localhost',
-      protocol: 'ws',
-    },
-  },
-  build: {
-    minify: true,
-    manifest: true,
-    rollupOptions: {
-      input: './src/main.js',
-      output: {
-        format: 'es',
-        entryFileNames: 'main.js',
-        esModule: true,
-        compact: true,
-        globals: {
-          jquery: '$',
+    plugins: [glsl()],
+    server: {
+        host: 'localhost',
+        cors: '*',
+        hmr: {
+            host: 'localhost',
+            protocol: 'ws',
         },
-      },
-      external: ['jquery'],
     },
-  },
-})
+    build: {
+        minify: true,
+        manifest: true,
+        rollupOptions: {
+            input: './src/main.js',
+            output: {
+                format: 'es', // Keep as ES module format
+                entryFileNames: 'main.js',
+                esModule: true,
+                compact: true,
+                globals: {
+                    jquery: '$',
+                },
+            },
+            external: ['jquery'],
+            // Disable code splitting
+            manualChunks: () => 'main.js', 
+        },
+    },
+});
