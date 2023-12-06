8175a7068fca0555d6b9cf41bbab4e5d48bbdedf
sas-miles <132398659+sas-miles@users.noreply.github.com>
2023-12-06 14:22:57 -0500

    more testing reverted back

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,32 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 import glsl from 'vite-plugin-glsl';
 
+// vite.config.js
 export default defineConfig({
-    plugins: [glsl()],
-    server: {
-        host: 'localhost',
-        cors: '*',
-        hmr: {
-            host: 'localhost',
-            protocol: 'ws',
-        },
+  plugins: [glsl()],
+  server: {
+    host: 'localhost',
+    cors: '*',
+    hmr: {
+      host: 'localhost',
+      protocol: 'ws',
     },
-    build: {
-        minify: true,
-        manifest: true,
-        rollupOptions: {
-            input: './src/main.js',
-            output: {
-                format: 'es', // Keep as ES module format
-                entryFileNames: 'main.js',
-                esModule: true,
-                compact: true,
-                globals: {
-                    jquery: '$',
-                },
-            },
-            external: ['jquery'],
-            // Disable code splitting
-            manualChunks: () => 'main.js', 
+  },
+  build: {
+    minify: true,
+    manifest: true,
+    rollupOptions: {
+      input: './src/main.js',
+      output: {
+        format: 'umd',
+        entryFileNames: 'main.js',
+        esModule: false,
+        compact: true,
+        globals: {
+          jquery: '$',
         },
+      },
+      external: ['jquery'],
     },
-});
+  },
+})
